874715199c66671dafaf6eb9b8bf98f5a0ae1459
Bulat Aikaev <mefcorvi@gmail.com>
2024-10-22 17:34:44 -0600

    chore: Update mexexam assets and dependencies

--- mexexam/assets/GeneralMenu-BmNPLncD.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./GeneralPage-Dg0V3b8_.js";import{o as n,c as o,I as t,n as c}from"./index-C8Ap_B6C.js";const a="_page_5jlit_1",l="_menu_5jlit_4",r={page:a,menu:l},_={};function m(e,i){return n(),o("div",{class:c(e.$style.menu)},[t(e.$slots,"default")],2)}const u={$style:r},f=s(_,[["render",m],["__cssModules",u]]);export{f as G};
